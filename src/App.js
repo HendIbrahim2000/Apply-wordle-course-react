54d835b8c8af07517d7d9016c5d846fe0b41dcb9
Hend Ibrahim <Hend.Ibrahim@Intellisec-me.com>
2022-08-02 12:15:25 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react'
+import Wordle from './Components/Wordle';
+import {useWordle} from './hooks/useWordle';
 
-function App() {
+function App () {
+  const [solution, setSolution] = useState(null)
+
+  useEffect(()=>{
+    return () => {
+      fetch("http://localhost:3001/solutions")
+      .then(res => res.json())
+      .then(result=>{
+        const randomSolution = result[Math.floor(Math.random() * 14)].word
+        setSolution(randomSolution)
+        // console.log(randomSolution)
+    })
+    
+    }
+  },[setSolution])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to wordle</h1>
+      {solution && <Wordle solution={solution} />}
     </div>
   );
 }
